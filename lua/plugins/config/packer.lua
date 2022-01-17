023dda311c537c847b953206df9115ca0a87a950
Bernt Anker <bernt.anker@rs.no>
2022-01-17 09:54:22 +0100

    Added floating window

--- lua/plugins/config/packer.lua
+++ lua/plugins/config/packer.lua
@@ -1,3 +1,7 @@
-return { display = {
-	open_fn = require("packer.util").float,
-} }
+return {
+	display = {
+		open_fn = function()
+			return require("packer.util").float({ border = "rounded" })
+		end,
+	},
+}
